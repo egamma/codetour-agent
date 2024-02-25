35e796c47682f5d5ff9cba159b0e77413a76e15b
egamma <egamma@microsoft.com>
2024-02-25 14:50:23 +0100

    Update to latest Chat API

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 
 const START_TOUR_COMMAND_ID = 'tour.startTour';
 
-interface ITourAgentResult extends vscode.ChatAgentResult2 {
+interface ITourAgentResult extends vscode.ChatResult {
 	tour: string;
 }
 
@@ -33,7 +33,7 @@ export function activate(context: vscode.ExtensionContext) {
 		let code = editor.document.getText();
 		return prefixLinesWithLineNumber(code, 1);
 	}
-	
+
 	function getSelectionCode(): string {
 		if (!vscode.window.activeTextEditor) {
 			return '';
@@ -43,8 +43,8 @@ export function activate(context: vscode.ExtensionContext) {
 		let code = editor.document.getText(selection.with({ start: selection.start.with({ character: 0 }), end: selection.end.with({ character: editor.document.lineAt(selection.end.line).text.length }) }));
 		return prefixLinesWithLineNumber(code, selection.start.line + 1);
 	}
-	
-	const handler: vscode.ChatAgentHandler = async (request: vscode.ChatAgentRequest, context: vscode.ChatAgentContext, stream: vscode.ChatAgentResponseStream, token: vscode.CancellationToken): Promise<ITourAgentResult> => {
+
+	const handler: vscode.ChatRequestHandler = async (request: vscode.ChatRequest, context: vscode.ChatContext, stream: vscode.ChatResponseStream, token: vscode.CancellationToken): Promise<ITourAgentResult> => {
 		if (request.command == 'explainEditor') {
 			let tour = await explainCode(request, token, stream, getFullCode);
 			return { tour: tour };
@@ -57,11 +57,10 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	};
 
-	const agent = vscode.chat.createChatAgent('codetour', handler);
-	agent.iconPath = vscode.Uri.joinPath(context.extensionUri, 'codetour.png');
-	agent.description = vscode.l10n.t('Answer questions with a code tour');
-	agent.fullName = vscode.l10n.t('CodeTour');
-	agent.commandProvider = {
+	const codeTourParticipant = vscode.chat.createChatParticipant('codetour', handler);
+	codeTourParticipant.iconPath = vscode.Uri.joinPath(context.extensionUri, 'codetour.png');
+	codeTourParticipant.description = vscode.l10n.t('Answer questions with a code tour');
+	codeTourParticipant.commandProvider = {
 		provideCommands(token) {
 			return [
 				{ name: 'explainEditor', description: 'Explain the code in the active editor with a code tour' },
@@ -70,39 +69,37 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	};
 
-	async function explainCode(request: vscode.ChatAgentRequest, token: vscode.CancellationToken, stream: vscode.ChatAgentResponseStream, getCode: () => string): Promise<string> {
+	async function explainCode(request: vscode.ChatRequest, token: vscode.CancellationToken, stream: vscode.ChatResponseStream, getCode: () => string): Promise<string> {
 		if (!vscode.window.activeTextEditor) {
 			vscode.window.showInformationMessage(`There is no active editor, open an editor and try again.`);
-			return '' ;
+			return '';
 		}
-		const access = await vscode.chat.requestLanguageModelAccess(LANGUAGE_MODEL_ID);
+		const access = await vscode.lm.requestLanguageModelAccess(LANGUAGE_MODEL_ID);
 
 		const filePath = getRelativeFilePath();
 		let lineNumberPrefixed = getCode();
 
 		const messages = [
-			{
-				role: vscode.ChatMessageRole.System,
-				content: `There is a VSCode extension called CodeTour it supports to create so called code tours in a JSON file.\n` +
-					`When asked to explain the code you should respond with a code tour JSON file.\n` +
-					`The JSON schema of the code tour file is "https://aka.ms/codetour-schema"\n` +
-					`Only respond with the contents of the code tour file as JSON and nothing else.\n` +
-					`When creating the tour then follow these guidelines:\n` +
-					`- Add a 'title' attribute to the tour that describes its purpose.\n` +
-					`- Add a 'file' attribute in each step use the following file path ${filePath} for its value.\n` +
-					`- Each line in the code starts with a comment that corresponds to the line number in the file.\n` +
-					`When creating a step for the tour use the line number from the comment at the beginning of the line.\n` +
-					- `Provide a detailed description for each step. The description may include markdown to improve its readability.\n` +
-					- `Only create on Code Tourstep for each line of code that you want to explain.\n`	
+			new vscode.LanguageModelSystemMessage(
+				`There is a VSCode extension called CodeTour it supports to create so called code tours in a JSON file.\n` +
+				`When asked to explain the code you should respond with a code tour JSON file.\n` +
+				`The JSON schema of the code tour file is "https://aka.ms/codetour-schema"\n` +
+				`Only respond with the contents of the code tour file as JSON and nothing else.\n` +
+				`When creating the tour then follow these guidelines:\n` +
+				`- Add a 'title' attribute to the tour that describes its purpose.\n` +
+				`- Add a 'file' attribute in each step use the following file path ${filePath} for its value.\n` +
+				`- Each line in the code starts with a comment that corresponds to the line number in the file.\n` +
+				`When creating a step for the tour use the line number from the comment at the beginning of the line.\n` +
+				- `Provide a detailed description for each step. The description may include markdown to improve its readability.\n` +
+				- `Only create on Code Tourstep for each line of code that you want to explain.\n`
 
-			},
-			{
-				role: vscode.ChatMessageRole.User,
-				content: `Create tour for the following question:\n` +
-					`${request.prompt}\n` +
-					`This is the code that you should explain in a tour\n.` +
-					`${lineNumberPrefixed}`
-			},
+			),
+			new vscode.LanguageModelUserMessage(
+				`Create tour for the following question:\n` +
+				`${request.prompt}\n` +
+				`This is the code that you should explain in a tour\n.` +
+				`${lineNumberPrefixed}`
+			),
 		];
 
 		const chatRequest = access.makeChatRequest(messages, {}, token);
@@ -142,7 +139,7 @@ export function activate(context: vscode.ExtensionContext) {
 	async function startTour(tour: any) {
 		const extensionId = 'vsls-contrib.codetour';
 		const extension = vscode.extensions.getExtension(extensionId);
-	
+
 		if (!extension) {
 			console.log(`Failed to get extension: ${extensionId}`);
 			return;
@@ -150,11 +147,11 @@ export function activate(context: vscode.ExtensionContext) {
 		if (!extension.isActive) {
 			await extension.activate();
 		}
-	
+
 		const api = extension.exports;
 		api.startTour(tour);
 	}
-	
+
 	function validateTour(tour: any): boolean {
 		if (!tour || !tour.title || !tour.steps || !Array.isArray(tour.steps)) {
 			return false;
@@ -181,7 +178,7 @@ export function activate(context: vscode.ExtensionContext) {
 	}
 
 	context.subscriptions.push(
-		agent,
+		codeTourParticipant,
 		vscode.commands.registerCommand(START_TOUR_COMMAND_ID, async (arg) => {
 			try {
 				let tour = JSON.parse(arg);
